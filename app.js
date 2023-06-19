1c21410297dad523b18b05fe0cca173fe3e93a71
smyn95 <tlsalduszz@gmail.com>
2023-06-19 09:06:44 +0900

    게시글 CRUD 구현

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const postRouter = require("./post");
+
 const app = express();
 const port = 3005;
 
+// 미들웨어 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 라우터 설정
+app.use("/post", postRouter);
+
+// 기본 경로 핸들러
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// 서버 시작
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
