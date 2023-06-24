a29399ef8db2f3182918ff96e791849143743af6
smyn95 <tlsalduszz@gmail.com>
2023-06-24 19:13:39 +0900

    CRUD 코드 수정 및 미들웨어 추가

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const postRouter = require("./post");
+
 const app = express();
 const port = 3005;
 
-app.use("/post", postRouter);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(`[${req.method}] ${req.path} | ${new Date().toLocaleString()}`);
+  next();
+});
+
+app.use("/post", postRouter);
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
 });
 
+app.use((error, req, res, next) => {
+  console.error(error.stack);
+  res.status(500).send({ message: "Internal Server Error" });
+});
+
+app.all("*", (req, res) => {
+  res.status(405).send({ message: "Invalid Route" });
+});
+
 app.listen(port, () => {
-  console.log(`Start`);
+  console.log(`Server started on port ${port}`);
 });
